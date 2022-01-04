733fc6e8977b4a9c681a68b2c8156df0e2a0802f
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-04 18:33:41 +0530

    Create createasg.tf

---
+++ createasg.tf
@@ -0,0 +1,17 @@
+resource "aws_launch_template" "foobar" {
+  name_prefix   = "foobar"
+  image_id      = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["us-east-1c"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.foobar.id
+    version = "$Latest"
+  }
+}
