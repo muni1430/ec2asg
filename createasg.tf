5bafe00ba13c687e7c65e93636ef9dd00bbd6324
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-05 12:58:21 +0530

    Update createasg.tf

--- createasg.tf
+++ createasg.tf
@@ -6,6 +6,7 @@ resource "aws_launch_template" "foobar" {
   name_prefix   = "foobar"
   image_id      = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
+  user_data = "${file("userdata.sh")}
 }
 resource "aws_autoscaling_group" "bar" {
   availability_zones = ["us-east-1c"]
