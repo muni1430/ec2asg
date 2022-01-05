fcd6dd94314d428d999d170090c7ec4b12f1378a
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-05 13:21:16 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
-#!/bin/bash\n
-sudo apt-get update -y\n
-sudo apt-get upgrade -y\n
-sudo apt-get install apache2 -y\n
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install apache2 -y
