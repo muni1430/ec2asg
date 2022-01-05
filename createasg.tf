aa1a4b6efaec8c8807c83fd73e557b0603ca658a
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-05 13:59:21 +0530

    Update createasg.tf

--- createasg.tf
+++ createasg.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 resource "aws_launch_template" "foobar" {
   name_prefix   = "foobar"
-  ami     = "ami-04505e74c0741db8d"
+  image_id      = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   user_data = "${file("userdata.sh")}"
 }
