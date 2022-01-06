6e4f20e19dd1148444e90cf107f5be2b3ae8c5a2
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-07 00:07:38 +0530

    Update createasg.tf

--- createasg.tf
+++ createasg.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "web" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   vpc_security_group_ids = [
-    "sg-0025ec649683001e8"
+    "sg-03769a97218bb6646"
   ]
   user_data = "${file("userdata.sh")}"
   subnet_id = "subnet-0991fd811575b0586"
